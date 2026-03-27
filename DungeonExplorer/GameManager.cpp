1a248225a26f19b68ccfe6754b3714799a07989f
geoni-ara <lgeeckh@gmail.com>
2026-03-27 14:23:51 +0900

    Feat: GameManger 추가, 몬스터 생성, 플레이어 생성 추가
    Gamemanager singleton으로 구현
    이름을 받아 플레이어를 생성할 수 있도록 구현
    레벨을 인자로 받아 몬스터를 생성할 수 있도록 구현

---
+++ DungeonExplorer/GameManager.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <string>
+#include "GameManager.h"
+#include "Goblin.h"
+#include "Character.h"
+
+using namespace std;
+
+// Constructor
+GameManager::GameManager(): enemy(nullptr), player(nullptr) {
+	// Move this to another place such as main
+	/*
+	string name;
+	cout << "Input Player Name: ";
+	// Enter name allowing spces
+	while(true) {
+		getline(cin, name);
+
+		//Remove leading whitespaces from the string
+		size_t startBlank = name.find_first_not_of(" \t\n\r\f\v");
+
+		// if the input contains only whitespace
+		if (startBlank == string::npos) {
+			cout  << "is InValid value\n Input Player Name: ";
+		}
+		else {
+			// Store the input after removing leading whitespace
+			name = name.substr(startBlank);
+			cout << "Your Name is " << name << "\n";
+			break;
+		}
+	}
+	*/
+}
+
+// Destructor
+GameManager::~GameManager() {
+	if (enemy != nullptr) delete enemy;
+	if (player != nullptr) delete player;
+}
+
+// Create the player only if it does not exist
+void GameManager::createPlayer(string name) {
+	if (player != nullptr) {
+		cout << "The player already exists.\n ";
+		return;
+	}
+	player = new Character(name);
+}
+
+// SingleTon - Created only once
+GameManager& GameManager::GetInstance() {
+	static GameManager instance;
+	return instance;
+}
+
+void GameManager::generateMonster(int level) {
+	// if no level is given, use the player's level
+	if (level == 0) level = player->GetLevel();
+
+	// Create a monster only if there is no current monster
+	// To-Do -> The monster type will be determined based on the player's current level
+	if (enemy == nullptr && player != nullptr) enemy = new Goblin("Goblin", 0, 0);
+}
+
---
+++ DungeonExplorer/GameManager.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+class Monster;
+class Character;
+
+class GameManager {
+private:
+	GameManager();
+
+	GameManager(const GameManager&) = delete;
+	GameManager& operator=(const GameManager&) = delete;
+
+	Monster* enemy;
+	Character* player;
+public:
+	static GameManager& GetInstance();
+
+	~GameManager();
+
+	void createPlayer(std::string name = "player");
+
+	void generateMonster(int level = 0);
+
+	void battle(Character* player);
+
+	void displayInventory(Character* player);
+
+	Monster* getEnemy() {
+		return enemy;
+	}
+
+	Character* getPlayer() {
+		return player;
+	}
+};
