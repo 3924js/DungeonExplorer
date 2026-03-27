1c7df58f407491e5550c3a50b144cf19d9d5fe6b
user <user>
2026-03-27 19:20:51 +0900

    add monster

--- DungeonExplorer/CreateMonster.cpp
+++ DungeonExplorer/CreateMonster.cpp
@@ -1,7 +1,7 @@
 #include "CreateMonster.h"
 
 Monster* CreateMonster::createGoblin(std::string name, int level) {
-	auto& gen = getGenerator();
+	auto& gen = MakeRandom::getGenerator();
 
 	std::uniform_int_distribution<int> hpDist(level * 20, level * 30);
 	std::uniform_int_distribution<int> atkDist(level * 5, level * 10);
@@ -9,4 +9,10 @@ Monster* CreateMonster::createGoblin(std::string name, int level) {
 	int atk = atkDist(gen);
 
 	return new Goblin(name, hp, atk);
+}
+
+Monster* CreateMonster::create(MonsterType type, std::string name, int level) {
+	switch (type) {
+	case MonsterType::Goblin: return createGoblin(name, level);
+	}
 }
--- DungeonExplorer/CreateMonster.h
+++ DungeonExplorer/CreateMonster.h
@@ -2,14 +2,12 @@
 #include <random>
 #include <string>
 #include "Goblin.h"
-class CreateMonster {
-private:
-	static inline std::mt19937& getGenerator() {
-		static std::random_device rd;
-		static std::mt19937 gen(rd());
-		return gen;
-	}
-public:
-	static Monster* createGoblin(std::string name, int level);
+#include "MakeRandom.h"
+enum class MonsterType{Goblin,Orc,Slime};
 
+class CreateMonster {
+public:
+	static Monster* create(MonsterType type, std::string name, int level);
+	static Monster* createGoblin(std::string name, int level);
+	
 };
